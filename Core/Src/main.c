3de123fdb86819faa387218cd77d4c285906c949
naoto <naoto@naoto-ThinkPad-X230>
2020-10-03 23:43:15 +0900

    Test limit SW_X1 (not use interrupt)

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -104,6 +104,12 @@ int main(void)
   /* USER CODE BEGIN WHILE */
   while (1)
   {
+	  if(HAL_GPIO_ReadPin(LIMIT_SW_X1_GPIO_Port, LIMIT_SW_X1_Pin)){
+		  HAL_GPIO_WritePin(LED_BUILTIN_GPIO_Port, LED_BUILTIN_Pin, 1);
+	  }else{
+		  HAL_GPIO_WritePin(LED_BUILTIN_GPIO_Port, LED_BUILTIN_Pin, 0);
+	  }
+	  HAL_Delay(10);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -336,17 +342,18 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : LIMIT_SW_X1_Pin LIMIT_SW_X2_Pin */
-  GPIO_InitStruct.Pin = LIMIT_SW_X1_Pin|LIMIT_SW_X2_Pin;
-  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-  GPIO_InitStruct.Pull = GPIO_NOPULL;
-  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
-
-  /*Configure GPIO pins : LIMIT_SW_Y1_Pin LIMIT_SW_Y2_Pin */
-  GPIO_InitStruct.Pin = LIMIT_SW_Y1_Pin|LIMIT_SW_Y2_Pin;
+  /*Configure GPIO pins : LIMIT_SW_X1_Pin LIMIT_SW_X2_Pin LIMIT_SW_Y1_Pin LIMIT_SW_Y2_Pin */
+  GPIO_InitStruct.Pin = LIMIT_SW_X1_Pin|LIMIT_SW_X2_Pin|LIMIT_SW_Y1_Pin|LIMIT_SW_Y2_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
   GPIO_InitStruct.Pull = GPIO_PULLDOWN;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI9_5_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI9_5_IRQn);
+
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
 }
 
